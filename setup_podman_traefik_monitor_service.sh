6ebddb44dbe0ba27832d29dff2b577c724ff31ac
stefano-ubuntuworkstation02 <stefano-ubuntuworkstation02@stefano.home>
2024-02-23 06:44:52 +0100

    Updating installation script for Traefik Monitoring Service to install to user folder, even if/when script is executed as root.

--- setup_podman_traefik_monitor_service.sh
+++ setup_podman_traefik_monitor_service.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
-# Tools Path
-toolspath=$(dirname $0)
+# Determine toolpath if not set already
+relativepath="./" # Define relative path to go from this script to the root level of the tool
+if [[ ! -v toolpath ]]; then scriptpath=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ); toolpath=$(realpath --canonicalize-missing $scriptpath/$relativepath); fi
+
+# Define user
+user=${1:-'podman'}
+
+# Get user home folder
+userhomedir=$( getent passwd "$user" | cut -d: -f6 )
+
+# Echo
+echo "Setup Traefik Monitoring Service for User <$user>"
 
 # Copy Traefik Monitoring Script to Podman User Folder
 mkdir -p ~/bin
-cp $toolspath/bin/monitor-traefik.sh ~/bin/monitor-traefik.sh
+cp $toolpath/bin/monitor-traefik.sh $userhomedir/bin/monitor-traefik.sh
 
 # Give Script Execution Permissions
-chmod +x ~/bin/monitor-traefik.sh
+chmod +x $userhomedir/bin/monitor-traefik.sh
+
+# Echo
+echo "Installing Systemd Service file in <$userhomedir/.config/systemd/user/monitor-traefik.service>"
 
 # Copy Traefik Monitoring Service File to Podman Systemd Service Folder
-cp $toolspath/systemd/services/monitor-traefik.service ~/.config/systemd/user/monitor-traefik.service
+cp $toolpath/systemd/services/monitor-traefik.service $userhomedir/.config/systemd/user/monitor-traefik.service
 
 # Reload Systemd Service Files
-systemctl --user daemon-reload
+runuser -l $user -c "systemctl --user daemon-reload"
 
 # Enable the Service to start automatically at each boot
-systemctl --user enable monitor-traefik.service
+runuser -l $user -c "systemctl --user enable monitor-traefik.service"
 
 # Start the Service
-systemctl --user restart monitor-traefik.service
+runuser -l $user -c "systemctl --user restart monitor-traefik.service"
 
 # Verify the Status is OK
-systemctl --user status monitor-traefik.service
+runuser -l $user -c "systemctl --user status monitor-traefik.service"
 
 # Check the logs from time to time and in case of issues
-journalctl --user -xeu monitor-traefik.service
+runuser -l $user -c "journalctl --user -xeu monitor-traefik.service"
