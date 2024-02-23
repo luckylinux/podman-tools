9625c08362280ad1f91a9cc3ef1ccea236287ab2
luckylinux <luckylinux>
2024-02-23 20:54:20 +0100

    Tidied up a bit the repository. Removed create_container.sh that wasn't doing anything at all. Moved get_podman_containers_uptime.sh to a playground subfolder, since that was the base for the traefik monitoring service, but it's already implemented there (no further need here).

--- create_container.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-read -p "Enter Container Name: " cname
-
-podman run --name ${cname} -p 80:80 -d ${cname}:latest
--- setup_podman_zfs_pool.sh
+++ setup_podman_zfs_pool.sh
@@ -1,2 +1,3 @@
-#zpool create -f -o ashift=12 -O atime=off -O canmount=off -O compression=lz4 -O mountpoint=/zdata zdata /dev/disk/by-partlabel/PODMAN
+# The Disk / Partition mentioned here it's actually a ZFS ZVOL on Proxmox VE passed through the PodmanServer## Virtual Machine
+# Thus there is no need to enable compression, as that task is already performed on the host, and would only bring extra performance loss
 zpool create -f -o ashift=12 -O atime=off -O canmount=off -O compression=off -O mountpoint=/zdata zdata /dev/disk/by-partlabel/PODMAN
