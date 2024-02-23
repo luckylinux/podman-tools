a190760655aab0b5b3cacd4053f42f2686ec907b
Stefano <stefano@UbuntuWorkstation11.stefano.home>
2024-02-23 17:47:27 +0100

    Debating whether to use CRON or Systemd Timers. Apparently podman-auto-update had already a timer defined ... Further investigation needed prior to deploying a further timer

--- setup_podman_auto_update.sh
+++ setup_podman_auto_update.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+# Determine toolpath if not set already
+relativepath="./" # Define relative path to go from this script to the root level of the tool
+if [[ ! -v toolpath ]]; then scriptpath=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ); toolpath=$(realpath --canonicalize-missing $scriptpath/$relativepath); fi
+
+# Create CRON file
+tee /etc/cron.d/podman-auto-update << EOF
+SHELL=/bin/bash
+0 0 * * * podman bash $toolpath/update_podman_containers.sh
+
+EOF
+
+# Make it executable
+chmod +x /etc/cron.d/podman-auto-update
+
 # Copy CRON file
-cp cron/podman-auto-update /etc/cron.d/podman-auto-update
+#cp cron/podman-auto-update /etc/cron.d/podman-auto-update
