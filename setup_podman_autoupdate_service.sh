0a074c3c2c150ce72803336cac34f8b2bdbce3ec
stefano-ubuntuworkstation02 <stefano-ubuntuworkstation02@stefano.home>
2024-02-24 19:19:38 +0100

    Use toolpath in order to load required functions, to make sure it's in the same folder as the script itself and not the current working folder !

--- setup_podman_autoupdate_service.sh
+++ setup_podman_autoupdate_service.sh
@@ -5,7 +5,7 @@ relativepath="./" # Define relative path to go from this script to the root leve
 if [[ ! -v toolpath ]]; then scriptpath=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ); toolpath=$(realpath --canonicalize-missing $scriptpath/$relativepath); fi
 
 # Load functions
-source functions.sh
+source $toolpath/functions.sh
 
 # Define user
 if [[ ! -v user ]]
