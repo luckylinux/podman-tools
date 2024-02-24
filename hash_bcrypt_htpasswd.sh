25a511e678d201bda42f760e785a735e80d0a846
stefano-ubuntuworkstation02 <stefano-ubuntuworkstation02@stefano.home>
2024-02-24 18:12:35 +0100

    Adding some spaces in bcrypt hash generator script for traefik basic authentication.

--- hash_bcrypt_htpasswd.sh
+++ hash_bcrypt_htpasswd.sh
@@ -4,7 +4,12 @@ read -p "Enter Username: " username
 
 read -s -p "Enter Password: " password
 
+echo -e "\n"
+
 htpasswd -bnBC 10 $username $password
+
+echo -e "\n"
 
 htpasswd -bnBC 10 "" $password | tr -d ':\n'
 
+echo -e "\n"
