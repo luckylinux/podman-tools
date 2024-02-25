792d4a1705457d85fbed226e9b4276f9739aa463
stefano-ubuntuworkstation02 <stefano-ubuntuworkstation02@stefano.home>
2024-02-25 09:16:59 +0100

    Force pull of new images and restart systemd podman container services, to try to force update.

--- update_podman_containers.sh
+++ update_podman_containers.sh
@@ -1,5 +1,52 @@
 #!/bin/bash
 
+# Save current pwd
+currentpath=$(pwd)
+
+# Determine toolpath if not set already
+relativepath="./" # Define relative path to go from this script to the root level of the tool
+if [[ ! -v toolpath ]]; then scriptpath=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ); toolpath=$(realpath --canonicalize-missing $scriptpath/$relativepath); fi
+
+# Load functions
+source $toolpath/functions.sh
+
+# Get user homedir
+userhomedir=$(get_homedir "$user")
+
+# Get Systemdconfigdir
+systemdconfigdir=$(get_systemdconfigdir "$user")
+
+# Prune old images
+podman image prune
+
+# List all images
+mapfile -t images < <( podman images --all --format="{{.Names}}" )
+
+# Iterate over images
+for image in "${images[@]}"
+do
+    # Remove Square brackets []
+    source=$(echo $image | | sed 's/[][]//g')
+
+    # If source is non-null then try to pull image
+    if [[ -v source ]]
+    then
+        # Pull (new ?) image
+        podman pull $source
+    fi
+done
+
+# Restart Systemd Container Services
+cd $systemdconfigdir
+for service in "container-*.service"
+do
+    # Restart container
+    systemd_restart "$user" "$service"
+done
+
+# Change back to currentpath
+cd $currentpath
+
 # Restart the podman-auto-update.service Systemd Service
 # This forces old images to be purges and news ones to be fetched
 systemctl --user restart podman-auto-update.service
