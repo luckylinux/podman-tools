0847895e8dbb82a5782934963a7d35075c7c3818
luckylinux <luckylinux>
2024-04-11 11:00:39 +0200

    Fixing syntax error for pull_policy validity check.

--- configure_pull_policy.sh
+++ configure_pull_policy.sh
@@ -23,9 +23,9 @@ pullpolicy=$3
 #pullpolicy=${3-"missing"}
 
 # Check if pull_policy is valid
-if [[ "${pullpolicy}" ~= "always" ]] && [[ "${pullpolicy}" ~= "missing" ]]
+if [[ "${pullpolicy}" != "always" ]] && [[ "${pullpolicy}" != "missing" ]]
 then
-   echo "ERROR: pull_policy must be one of: <always>, <missing>. Aborting."
+   echo "ERROR: pull_policy must be one of: <always>, <missing>. Entered value was <${pullpolicy}>. Aborting."
    exit 9
 fi
 
