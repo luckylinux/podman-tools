f04b5bd2b0b0d2b8f5b8b65832b9673db19a64e8
luckylinux <luckylinux>
2024-04-14 10:37:44 +0200

    Improved Migration Script to properly change Configuration.

--- migrate_podman_directory.sh
+++ migrate_podman_directory.sh
@@ -22,6 +22,10 @@ sourcedir=$2
 destinationdir=$3
 #destinationdir=${3-"/home/podman/containers"}
 
+# Create Destination Directory if Not Existing Yet
+mkdir -p ${destinationdir}
+chown -R $user:$user ${destinationdir}
+
 # Storage.conf File Location
 # MUST still be available after all things have been unmounted
 configrealpath=$4
@@ -79,31 +83,49 @@ do
         # Convert dataset name to lowercase mountpoint
         lname=${dataset,,}
 
+        # Source Path
+        sourcepath="${sourcedir}/${lname}"
+
+        # Destination Path
+        destionationpath="${destinationdir}/${lname}"
+
+        # If dataset does not exist yet, create it
+        if [[ ! -d "${sourcepath}" ]]
+        then
+           mkdir -p "${sourcepath}"
+           chown -R $user:$user $sourcedir/${lname}
+        fi
 
         # Make it editable
-        chattr -i $sourcedir/${lname}
+        chattr -i ${sourcepath}
 
         # Move mountpoint
-        mv $sourcedir/${lname} $destinationdir/${lname}
+        mv ${sourcepath} ${destinationpath}
+
+        # Give User Ownership
+        chown -R $user:$user ${destinationpath}
 
         # Require that a partition is mounted there again
-        chattr +i $destinationdir/${lname}
+        chattr +i ${destinationpath}
 
         # Make changes to /etc/fstab
-        sed -Ei "s|${sourcedir}/${lname}|${destinationdir}/${lname}|g" /etc/fstab.conf
-
-        # Make changes to storage.conf
-        sed -Ei "s|^#? ?graphroot = \".*\"|graphroot = \"${destinationdir}/${lname}\"|g" ${configrealpath}/storage.conf
-        sed -Ei "s|^#? ?rootless_storage_path = \".*\"|rootless_storage_path = \"${destinationdir}/${lname}\"|g" ${configrealpath}/storage.conf
-        sed -Ei "s|^#? ?imagestore = \".*\"|#imagestore = \"${destinationdir}/{$lname}\"|g" ${configrealpath}/storage.conf
-
-        # Make changes to registries.conf
-        # ...
-
-        # Make changes to containers.conf
-        sed -Ei "s|^#? ?volume_path = \".*\"|#volume_path = \"${destinationdir}/${lname}\"|g" ${configrealpath}/storage.conf
-        #sed -Ei "s|^#? ?volumepath = \".*\"|#volumepath = \"${destinationdir}/${lname}\"|g" ${configrealpath}/storage.conf
+        sed -Ei "s|${sourcepath}|${destinationpath}|g" /etc/fstab.conf
 done
+
+# Make changes to storage.conf
+sed -Ei "s|^#? ?graphroot = \".*\"|graphroot = \"${destinationdir}/storage\"|g" ${configrealpath}/storage.conf
+sed -Ei "s|^#? ?rootless_storage_path = \".*\"|rootless_storage_path = \"${destinationdir}/storage\"|g" ${configrealpath}/storage.conf
+
+# Using Imagestore gives problems so make sure to Disable it in the Process !
+sed -Ei "s|^#? ?imagestore = \".*\"|#imagestore = \"${destinationdir}/images\"|g" ${configrealpath}/storage.conf
+
+# Make changes to registries.conf
+# ...
+
+# Make changes to containers.conf
+# Also fix wrong "volumepath" syntax to the correct "volume_path"
+sed -Ei "s|^#? ?volumepath = \".*\"|volume_path = \"${destinationdir}/volumes\"|g" ${configrealpath}/storage.conf
+sed -Ei "s|^#? ?volume_path = \".*\"|volume_path = \"${destinationdir}/volumes\"|g" ${configrealpath}/storage.conf
 
 # Remount all mountpoints
 zfs mount -a
@@ -112,5 +134,9 @@ mount -a
 # Reset podman as user
 generic_cmd "$user" "podman" "system" "reset"
 
-# Remove remaining stuff in storage
+# Remove remaining stuff in storage and images
 rm -rf ${sourcedir}/storage/*
+rm -rf ${sourcedir}/images/*
+
+# Shoud Reboot
+echo "You should now Reboot !"
