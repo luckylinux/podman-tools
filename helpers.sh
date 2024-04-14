7c52664b345b582758ad63824160123fe819c6ba
luckylinux <luckylinux>
2024-04-14 15:31:50 +0200

    Added helper to update tools from any path.

--- helpers.sh
+++ helpers.sh
@@ -4,3 +4,29 @@ if [[ ! -v toolpath ]]; then scriptpath=$(cd "$( dirname "${BASH_SOURCE[0]}" )" 
 
 # Load Functions
 source $toolpath/functions.sh
+
+# Update Tools from anywhere
+update_tools() {
+   # The User is passed as Optional Argument
+   local luser=${1-""}
+   if [[ -z "$luser" ]]
+   then
+      luser=$(whoami)
+   fi
+
+   # Save Current Directory
+   currentpath=$(pwd)
+
+   # Change Directory to Toolpath
+   cd $toolpath
+
+   # Do a git pull
+   generic_cmd "${luser}" "git" "pull"
+
+   # Reload ~/.bash_profile
+   homedir=$(get_homedir "${luser}")
+   generic_cmd "${luser}" "source" "${$homedir}/.bash_profile"
+
+   # Go back to Current Path
+   cd $currentpath
+}
