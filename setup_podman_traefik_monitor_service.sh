94238856509629f5378d2461a53fc2f6e6fcfd7e
luckylinux <luckylinux>
2024-04-20 13:02:16 +0200

    Change Path for installation from ~/bin to ~/.local/bin

--- setup_podman_traefik_monitor_service.sh
+++ setup_podman_traefik_monitor_service.sh
@@ -5,7 +5,7 @@ relativepath="./" # Define relative path to go from this script to the root leve
 if [[ ! -v toolpath ]]; then scriptpath=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ); toolpath=$(realpath --canonicalize-missing $scriptpath/$relativepath); fi
 
 # Load functions
-source $toolpath/functions.sh
+source "${toolpath}/functions.sh"
 
 # Define user
 #user=$1
@@ -22,34 +22,37 @@ then
 fi
 
 # Get user home folder
-userhomedir=$( get_homedir "$user" )
+userhomedir=$( get_homedir "${user}" )
 
 # Get Systemd Config Folder
-systemdconfigfolder=$( get_systemdconfigdir "$user" )
+systemdconfigfolder=$( get_systemdconfigdir "${user}" )
+
+# Generate Path to Install Executable
+userbinpath="${userhomedir}/.local/bin"
 
 # Make sure that the Traefik Systemd Service has already been set up
-$toolpath/configure_podman_service_autostart.sh "traefik"
+${toolpath}/configure_podman_service_autostart.sh "traefik"
 
 # Define service name
 service="monitor-traefik.service"
 
 # Echo
-echo "Setup Traefik Monitoring Service for User <$user>"
+echo "Setup Traefik Monitoring Service for User <${user}>"
 
 # Copy Traefik Monitoring Script to Podman User Folder
-mkdir -p $userhomedir/bin
-cp $toolpath/bin/monitor-traefik.sh $userhomedir/bin/monitor-traefik.sh
-chown $user:$user $userhomedir/bin/monitor-traefik.sh
+mkdir -p "${userbinpath}"
+cp "${toolpath}/bin/monitor-traefik.sh" "${userbinpath}/monitor-traefik.sh"
+chown "${user}:${user}" "${userbinpath}/monitor-traefik.sh"
 
 # Give Script Execution Permissions
-chmod +x $userhomedir/bin/monitor-traefik.sh
+chmod +x "${userbinpath}/monitor-traefik.sh"
 
 # Echo
-echo "Installing Systemd Service file in <$systemdconfigfolder/$service>"
+echo "Installing Systemd Service file in <${systemdconfigfolder}/${service}>"
 
 # Copy Traefik Monitoring Service File to Podman Systemd Service Folder
-cp $toolpath/systemd/services/$service $systemdconfigfolder/$service
-chown $user:$user $systemdconfigfolder/$service
+cp "${toolpath}/systemd/services/${service}" "${systemdconfigfolder}/${service}"
+chown "${user}:${user}" "${systemdconfigfolder}/${service}"
 
 # Enable & Start Systemd file
-systemd_reload_enable "$user" "$service"
+systemd_reload_enable "${user}" "${service}"
