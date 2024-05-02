8e761ee369a2f17b963d57d1f081e1ecd7bec185
luckylinux <luckylinux>
2024-05-02 21:02:59 +0200

    Adding remark/note to file.

--- update_tools_configuration.sh
+++ update_tools_configuration.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+###################################################
+## IMPORTANT: THIS FILE MUST BE RUN **MANUALLY** ##
+###################################################
+
 # Determine toolpath if not set already
 relativepath="./" # Define relative path to go from this script to the root level of the tool
 if [[ ! -v toolpath ]]; then scriptpath=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ); toolpath=$(realpath --canonicalize-missing ${scriptpath}/${relativepath}); fi
