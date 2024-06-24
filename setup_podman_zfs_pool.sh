c89fab941f7306598c7f114f59d9af1379508601
luckylinux <luckylinux>
2024-06-24 19:19:12 +0200

    Use compatibility=openzfs-2.0-linux by default

--- setup_podman_zfs_pool.sh
+++ setup_podman_zfs_pool.sh
@@ -1,3 +1,3 @@
 # The Disk / Partition mentioned here it's actually a ZFS ZVOL on Proxmox VE passed through the PodmanServer## Virtual Machine
 # Thus there is no need to enable compression, as that task is already performed on the host, and would only bring extra performance loss
-zpool create -f -o ashift=12 -O atime=off -O canmount=off -O compression=off -O mountpoint=/zdata zdata /dev/disk/by-partlabel/PODMAN
+zpool create -f -o ashift=12 -O atime=off -O canmount=off -O compression=off -O mountpoint=/zdata -o compatibility=openzfs-2.0-linux zdata /dev/disk/by-partlabel/PODMAN
