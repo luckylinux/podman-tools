1ecb888949a1c43fae3b60ac06404772b2a1a7e2
luckylinux <luckylinux>
2025-12-30 20:17:21 +0100

    Try again with sudo on Alpine Linux.

--- setup_podman_compose_local.sh
+++ setup_podman_compose_local.sh
@@ -51,3 +51,6 @@ cd ${HOME} || exit
 
 # Setup venv for Podman Compose
 generic_cmd "${user}" bash -c "python3 -m venv \${HOME}/podman-compose ; source \${HOME}/podman-compose/bin/activate ; pip install --upgrade pip ; pip install --upgrade git+https://github.com/containers/podman-compose.git@v1.5.0"
+
+# This Command works on Alpine Linux
+# sudo -u podman -i bash -c "python3 -m venv \${HOME}/podman-compose ; source \${HOME}/podman-compose/bin/activate ; pip install --upgrade pip ; pip install --upgrade git+https://github.com/containers/podman-compose.git@v1.5.0"
