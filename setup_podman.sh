7e387eae3e225c41d1c52577b67020cec3ab9cf1
luckylinux <luckylinux>
2025-12-30 13:44:39 +0100

    Also add sudo for Alpine.

--- setup_podman.sh
+++ setup_podman.sh
@@ -408,7 +408,7 @@ then
 elif [[ "${distribution}" == "alpine" ]]
 then
     # Install Packages
-    apk add jq podman python3 podman-compose
+    apk add sudo jq podman python3 podman-compose
 else
     echo "[ERROR]: Distribution ${distribution} is NOT Supported. ABORTING !"
     exit 9
